051b402d4231764c370231249db820f13ea7b214
Nida Sagri <nidasagri@gmail.com>
2024-09-18 13:06:19 +0530

    first commit

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,54 +1,69 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { calculateTie, calculateWinner } from './gameLogic';
-import './TicTacToe.css'
+import './TicTacToe.css';
 
 const TicTacToe = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
 
-    const winner = calculateWinner(squares);
-    const isTie = calculateTie(squares);
+  const winner = calculateWinner(squares);
+  const isTie = calculateTie(squares);
 
-    const handleClick = (index)=>{
-        if(squares[index] || winner)
-            return;
+  const handleClick = (index) => {
+    if (squares[index] || winner) return;
 
-        const newSquares = squares.slice();
-        newSquares[index] = isXNext? 'X':'O';
-        setSquares(newSquares);
-        setIsXNext(!isXNext);
-    };
+    const newSquares = squares.slice();
+    newSquares[index] = isXNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+  };
 
-    const rendersquare = (index)=>(
-      <button className="square" onClick={() => handleClick(index)}>{squares[index]}</button>
-    );
 
-    const status = winner? `Winner: ${winner}`: isTie?"Its a Tie!" : `Next player: ${isXNext? 'X': 'O'}`;
+  const resetGame = () => {
+    setSquares(Array(9).fill(null)); 
+    setIsXNext(true); 
+  };
 
+  const renderSquare = (index) => (
+    <button className="square" onClick={() => handleClick(index)}>
+      {squares[index]}
+    </button>
+  );
+
+  const status = winner
+    ? `Winner: ${winner}`
+    : isTie
+    ? "It's a Tie!"
+    : `Next player: ${isXNext ? 'X' : 'O'}`;
 
   return (
     <div>
-    <h1 className="msg">Tic Tac Toe Game</h1>
+      <h1 className="msg">Tic Tac Toe Game</h1>
       <div className="status">{status}</div>
       <div className="board-row">
-        {rendersquare(0)}
-        {rendersquare(1)}
-        {rendersquare(2)}
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
 
       <div className="board-row">
-        {rendersquare(3)}
-        {rendersquare(4)}
-        {rendersquare(5)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
 
       <div className="board-row">
-        {rendersquare(6)}
-        {rendersquare(7)}
-        {rendersquare(8)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
+
+      <button id="resetBtn" onClick={resetGame}>
+        Reset
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default TicTacToe
+export default TicTacToe;
+
