3d441b598964eb861f2cd4992a5aa486bcce3e28
Nida Sagri <nidasagri@gmail.com>
2024-09-18 12:33:21 +0530

    first commit

---
+++ src/TicTacToe.js
@@ -0,0 +1,53 @@
+import React,{useState} from 'react'
+import { calculateTie, calculateWinner } from './gameLogic';
+import './TicTacToe.css'
+
+const TicTacToe = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
+
+    const winner = calculateWinner(squares);
+    const isTie = calculateTie(squares);
+
+    const handleClick = (index)=>{
+        if(squares[index] || winner)
+            return;
+
+        const newSquares = squares.slice();
+        newSquares[index] = isXNext? 'X':'O';
+        setSquares(newSquares);
+        setIsXNext(!isXNext);
+    };
+
+    const rendersquare = (index)=>(
+      <button className="square" onClick={() => handleClick(index)}>{squares[index]}</button>
+    );
+
+    const status = winner? `Winner: ${winner}`: isTie?"Its a Tie!" : `Next player: ${isXNext? 'X': 'O'}`;
+
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {rendersquare(0)}
+        {rendersquare(1)}
+        {rendersquare(2)}
+      </div>
+
+      <div className="board-row">
+        {rendersquare(3)}
+        {rendersquare(4)}
+        {rendersquare(5)}
+      </div>
+
+      <div className="board-row">
+        {rendersquare(6)}
+        {rendersquare(7)}
+        {rendersquare(8)}
+      </div>
+    </div>
+  )
+}
+
+export default TicTacToe
